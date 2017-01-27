f226ff75802d3d1f24dcafae2264690145fa76a2
wangyi2005 <wangyi2005@users.noreply.github.com>
2017-01-27 21:24:34 +0800

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+/opt/kcptun/client_linux_amd64 -r %(ENV_REMOTEADDR)s -l :%(ENV_KCP_PORT)s --crypt none --mode %(ENV_KCP_MODE)s --mtu %(ENV_MTU)s --sndwnd %(ENV_SNDWND)s --rcvwnd %(ENV_RCVWND)s --datashard %(ENV_DATASHARD)s --parityshard %(ENV_PARITYSHARD)s --nocomp
+
+
